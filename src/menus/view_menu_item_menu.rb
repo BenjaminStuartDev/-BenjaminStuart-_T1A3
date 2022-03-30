c33b4ef3aa5258fd5fa2ad8975a6c9b676035b02
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-03-30 17:04:32 +1000

    Fixed breaks

--- src/menus/view_menu_item_menu.rb
+++ src/menus/view_menu_item_menu.rb
@@ -35,7 +35,7 @@ class ViewMenuItemMenu < Menu
     else
       @table.orders << @menu_item
       puts "#{@menu_item.name} has been added to table #{@table.table_num}'s order."
-      @@breaks = 1
+      @@breaks = 2
     end
   end
 end

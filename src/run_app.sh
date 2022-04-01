200c8c361c3dd01dce3b0e817bc53e2fc8c5f988
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-04-01 13:55:25 +1000

    recommit

--- src/run_app.sh
+++ src/run_app.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+bundle install
 
 # check if savefile exists.
 # if savefile exists puts welcome back and request the owners name and password
 # if savefule does not exist greet the user and prompt them to setup the first manager on their account
 # run the program with first time arguments
+
 clear
 echo $1 "< - Initialising Pos - >"
+
 
 function savefile_exist? {
     FILE=./saves/savefile.json
     if test -f "$FILE"; then
-        echo $1 "Welcome back to your cafe POS!"
-        echo -e "Please enter your Name: "
-        read name
-        echo -e "Please enter your Password: "
-        read password
+        ruby index.rb
     else
         echo $1 "Hello and welcome to your cafe POS"
         echo $1 "We are now going to set up your first manager profile"
@@ -22,7 +21,7 @@ function savefile_exist? {
         read name
         echo -e "Please enter your Password: "
         read password
-    ruby index.rb $name $password  
+        ruby index.rb $name $password  
     fi
 }
 savefile_exist?
--- src/test.rb
+++
@@ -1,3 +0,0 @@
-require 'tty-table'
-table = TTY::Table.new(%w[header1 header2], [%w[a1 a2], %w[b1 b2]])
-# puts table.render(:ascii)
